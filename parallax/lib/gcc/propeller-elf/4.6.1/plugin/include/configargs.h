179e7652c88c751189dac848dbd0fe915c11504f
brunofavs <bruno.favs@ua.pt>
2023-04-18 01:47:32 +0200

    testing gitignore

--- parallax/lib/gcc/propeller-elf/4.6.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../../propgcc/gcc/configure --target=propeller-elf --prefix=/opt/parallax --disable-nls --disable-libssp --disable-lto --disable-shared --with-pkgversion=propellergcc_v1_0_0_2411 --with-bugurl=http://code.google.com/p/propgcc/issues : (reconfigured) ../../propgcc/gcc/configure --target=propeller-elf --prefix=/opt/parallax --disable-nls --disable-libssp --disable-lto --disable-shared --with-pkgversion=propellergcc_v1_0_0_2411 --with-bugurl=http://code.google.com/p/propgcc/issues";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
